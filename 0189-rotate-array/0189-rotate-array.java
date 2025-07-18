0768f40b1e313a4bc9036c14265b36b689fb0284
Shivangi Chaurasia <Shivangichaurasia1515@gmail.com>
2025-07-19 03:12:09 +0530

    Time: 0 ms (100%), Space: 57.6 MB (36.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,28 +1,46 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
-        int n=nums.length;
+        
 
+        int n=nums.length;
         k=k%n;
 
-        //store last k elements in temp 
-        int[]temp=new int[k];
-        for(int i=0;i<k;i++){
-            temp[i]=nums[n-k+i];
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
 
-        }
-        //start shifting 
-        for(int i=n-k-1;i>=0;i--){
-            nums[i+k]=nums[i];
-        }
-        //Now put temp elements at the front
-        for(int i=0;i<k;i++){
-            nums[i]=temp[i];
-        }
+        // k=k%n;
+
+        // //store last k elements in temp 
+        // int[]temp=new int[k];
+        // for(int i=0;i<k;i++){
+        //     temp[i]=nums[n-k+i];
+
+        // }
+        // //start shifting 
+        // for(int i=n-k-1;i>=0;i--){
+        //     nums[i+k]=nums[i];
+        // }
+        // //Now put temp elements at the front
+        // for(int i=0;i<k;i++){
+        //     nums[i]=temp[i];
+        // }
+
+
 
 
 
 
         
+    }
+    public void reverse(int[]nums,int start,int end){
+        while(start<=end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
     }
 }
