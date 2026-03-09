823d3f5cffb09fa757bd284c9f0453ae4ce7e83d
Shivangi Chaurasia <Shivangichaurasia1515@gmail.com>
2026-03-09 19:50:17 +0530

    Time: 0 ms (100%), Space: 41.6 MB (99.21%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        //By Recursion and Dynamic Programming
+
+        if(n==1 || n==2){ //Base Case
+            return n;
+        }
+
+        int[]dp=new int[n+1];
+
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+        
+    }
+}
