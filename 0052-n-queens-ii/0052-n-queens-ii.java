8c7c6195f95a3cce811629d7c180114b3582713e
Shivangi Chaurasia <Shivangichaurasia1515@gmail.com>
2026-03-16 12:49:33 +0530

    Time: 2 ms (61.37%), Space: 42.4 MB (18.31%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,62 @@
+class Solution {
+    int count=0;
+    public void nQueens(char[][]board,int row){
+        //base case
+        if(row==board.length){
+            count++;
+            return ;
+        }
+        // column loop
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+                nQueens(board,row+1); //function call
+                board[row][j]='.'; //backtracking step
+            }
+
+
+        }
+       
+
+    }
+     public boolean isSafe(char[][]board,int row,int col){
+        //vertical
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+
+
+
+        //left diagonal
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+
+
+        //right diagonal
+        for(int i=row-1,j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int totalNQueens(int n) {
+        char[][]board=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        nQueens(board,0);
+
+        return count;
+
+        
+    }
+}
