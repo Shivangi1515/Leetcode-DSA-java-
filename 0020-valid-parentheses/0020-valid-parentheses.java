c42b8a4cd33de49ef32c90284ccdd0c7faedcd12
Shivangi Chaurasia <Shivangichaurasia1515@gmail.com>
2026-04-12 00:30:58 +0530

    Time: 3 ms (87.72%), Space: 43.4 MB (46.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
 
+
         Stack<Character>st=new Stack<>();
 
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
 
-            if(ch=='(' ||ch=='{'||ch=='['){
+            if(ch=='{' || ch=='[' || ch=='('){
                 st.push(ch);
             }
             else{
@@ -15,16 +16,16 @@ class Solution {
                 }
                 if((st.peek()=='(' && ch==')') || (st.peek()=='{' && ch=='}') || (st.peek()=='[' && ch==']'))
                     {
-                        st.pop();
-                    }
-                else{
-                        return false;
-                    }
+                    st.pop();
                 }
+                else{
+                    return false;
+                }
+
+            }
             
         }
         return st.isEmpty();
-        
         
     }
 }
