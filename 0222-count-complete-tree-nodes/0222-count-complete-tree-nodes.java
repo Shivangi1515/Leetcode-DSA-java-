fad6f42b57be1a25dda30f98ac7883b5857a21b1
Shivangi Chaurasia <Shivangichaurasia1515@gmail.com>
2026-05-18 05:50:34 +0530

    Time: 0 ms (100%), Space: 49.6 MB (12.69%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,9 +16,11 @@
 class Solution {
     public int countNodes(TreeNode root) {
 
+
         if(root==null){
             return 0;
         }
+
 
         int lcount=countNodes(root.left);
         int rcount=countNodes(root.right);
